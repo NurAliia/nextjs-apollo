5716479973b05460a1586b53825cb070eee7c538
NurAliia <battalova.nur@gmail.com>
2021-07-13 16:13:57 +0300

    Set up app nextjs

---
+++ pages/data/index.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export default function HomePage() {
+  return <Title>Load Data</Title>;
+}
+
+const Title = styled.h1`
+  color: crimson;
+`;
+
+
+// export async function getServerSideProps() {
+//     const req = await fetch('https://rickandmortyapi.com/documentation/#graphql');
+
+// }
---
+++ pages/home.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export default function HomePage() {
+  return <Title>Home Page</Title>;
+}
+
+const Title = styled.h1`
+  color: crimson;
+`;
